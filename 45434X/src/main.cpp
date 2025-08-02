9196c597a99c1a8a51880b66e8540a40a5e6f3eb
uiGalaxyGalaxy <uigalaxyandromeda@gmail.com>
2025-08-01 22:17:02 -0400

    baseline code

--- 45434X/src/main.cpp
+++ 45434X/src/main.cpp
@@ -1,19 +1,99 @@
 #include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+// create the controller
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+
+
+// Initialize the Drivetrain:
+
+// Creates a Motor Group with PROS. the number is the port. Negative numbers = reverse
+
+pros::MotorGroup left_motors({1, 2, 3}, pros::MotorGearset::blue);
+pros::MotorGroup right_motors({4, 5, 6}, pros::MotorGearset::blue);
+
+//IMU = inertial sensor, number = port
+pros::Imu imu(10);
+
+//our rotation sensors for odom
+pros::Rotation horizontal(1);
+pros::Rotation vertical(1);
+
+// define our odom wheels (sensor, wheel, tracking offset)
+// how far away is the odom wheel from the center of the bot? for ex. if a horizontal sensor is 4 inches below the center, it would be -4.
+lemlib::TrackingWheel horizontal_odom(&horizontal, lemlib::Omniwheel::NEW_275, 0);
+lemlib::TrackingWheel vertical_odom(&vertical, lemlib::Omniwheel::NEW_275, 0);
+
+// init odomsensors to feed the chassis class
+lemlib::OdomSensors sensors(&vertical_odom, // vertical tracking wheel 1
+                            nullptr, // vertical tracking wheel 2
+                            &horizontal_odom, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+
+// Create the Drivetrain with LemLib.
+lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+                              &right_motor_group, // right motor group
+                              10, // The track width (From the front of the bot, how far apart are the wheels?)
+                              lemlib::Omniwheel::NEW_4, // what wheel?
+                              360, // Drivetrain RPM
+                              2 // horizontal drift
+);
+
+// DRIVING STUFF:
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
+);
+
+// finally create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors, // odometry sensors
+						&throttle_curve, 
+                        &steer_curve
+);
+
+
+
 void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+		pros::lcd::set_text(2, "paradox  is gay and proud ");
 }
 
 /**
@@ -23,10 +103,22 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	pros::lcd::initialize(); //initialize the screen
+	chassis.calibrate(); //calibrate sensors yippee
+
 
 	pros::lcd::register_btn1_cb(on_center_button);
+
+	pros::Task spitInfo([&]() {
+        while (true) {
+            pros::lcd::print(0, "X: %f", chassis.getPose().x);
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y);
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);
+            pros::delay(20);
+			//spits info onto the screen so you can perfect PID!
+			//this utilizes an Lambda: you name, define, and use the task all in one
+        }
+    });
 }
 
 /**
@@ -74,21 +166,17 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+while true(
+	    int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
+        // Curvature Drive
+        chassis.curvature(leftY, rightX);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+		// Arcade Drive (disable the curve exponent stuff, higher numbers = prioritize throttle)
+		//chassis.arcade(leftY, leftX, false, 0.75);
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+        // delay to save resources
+        pros::delay(25); 
+)
 }
